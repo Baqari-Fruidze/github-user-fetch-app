abc633602ee23132267a68c2a9dc74831a454f18
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-12 20:23:30 +0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import styled from "styled-components";
+import search from "/assets/icon-search.svg";
+
 function App() {
   return (
     <>
       <h1>hellloo</h1>
+      <img src={search} alt="" />
     </>
   );
 }
 
+const Cover = styled.div`
+  background: #141d2f;
+`;
 export default App;
