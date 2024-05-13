f9879b8249eb16d9d279346b92ce4175d223ea3a
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-13 22:19:53 +0400

    middle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import styled from "styled-components";
 import search from "/assets/icon-search.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Picture from "./components/Picture";
+
+type Tdata = {
+  login: string;
+  id: number;
+  avatar_url: string;
+  company: string;
+  twitter_username: string;
+  blog: string;
+  following: number;
+  location: string;
+  followers: number;
+  public_repos: string;
+  bio: string;
+  created_at: string;
+  name: string;
+};
 
 function App() {
   const [themee, setThemee] = useState<boolean>(true);
+  // const [data, setData] = useState();
+  const apiFetch = async () => {
+    const res = await fetch(`https://api.github.com/users/octocat`);
+    const info = await res.json();
+    // setData(info);
+    console.log(info);
+  };
+  apiFetch();
   return (
     <Cover themee={themee}>
       <Header themee={themee} setThemee={setThemee} />
+      <Picture />
     </Cover>
   );
 }
---
+++ src/components/Picture.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import website from "/assets/icon-website.svg";
+
+export default function Picture() {
+  return (
+    <div>
+      <img src={website} alt="" />
+    </div>
+  );
+}
