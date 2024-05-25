0c5223d71bca9aae9381fcac704d6b79b6830432
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-25 21:46:33 +0400

    mobile done

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import styled from "styled-components";
 import search from "/assets/icon-search.svg";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import Picture from "./components/Picture";
 import { Tdata } from "./components/types";
+import location from "/assets/icon-location.svg";
+import webSite from "/assets/icon-website.svg";
+import twitter from "/assets/icon-twitter.svg";
+import company from "/assets/icon-company.svg";
 
 function App() {
   const [themee, setThemee] = useState<boolean>(true);
@@ -36,30 +39,94 @@ function App() {
             : data.bio}
         </Para>
         <ReposFolowersCon themee={themee}>
-          <ReposCon>
-            <span>Repos</span>
+          <ReposCon themee={themee}>
+            <p>Repos</p>
             <span>{data.public_repos}</span>
           </ReposCon>
-          <ReposCon>
-            <span>Followers</span>
+          <ReposCon themee={themee}>
+            <p>Followers</p>
             <span>{data.followers}</span>
           </ReposCon>
-          <ReposCon>
-            <span>Following</span>
+          <ReposCon themee={themee}>
+            <p>Following</p>
             <span>{data.following}</span>
           </ReposCon>
         </ReposFolowersCon>
+        <LocationBottomCon>
+          <MiniCons themee={themee}>
+            <img src={location} alt="" />
+            <p>{data.location}</p>
+          </MiniCons>
+          <MiniCons themee={themee}>
+            <img src={webSite} alt="" />
+            <p>{data.blog}</p>
+          </MiniCons>
+          <MiniCons themee={themee}>
+            <img src={twitter} alt="" />
+            {data.twitter_username === null ? (
+              <p>Not Available</p>
+            ) : (
+              <p>{data.twitter_username}</p>
+            )}
+          </MiniCons>
+          <MiniCons themee={themee}>
+            <img src={company} alt="" />
+            {data.company === null ? (
+              <p>Not Available</p>
+            ) : (
+              <p>{data.company}</p>
+            )}
+          </MiniCons>
+        </LocationBottomCon>
       </InfoMainCon>
     </Cover>
   );
 }
 
-const ReposCon = styled.div`
+const MiniCons = styled.div<{ themee: boolean }>`
+  align-items: center;
+  display: flex;
+  gap: 2rem;
+  & p {
+    color: ${(props) => (props.themee ? "#4b6a9b" : "#fff")};
+    font-family: "Space Mono";
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+const LocationBottomCon = styled.div`
+  grid-column: 1/4;
+  display: flex;
+  flex-direction: column;
+  gap: 1.7rem;
+`;
+const ReposCon = styled.div<{ themee: boolean }>`
   align-items: center;
   justify-content: center;
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
+  & > p {
+    color: ${(props) => (props.themee ? "#4b6a9b" : "#fff")};
+    text-align: center;
+    font-family: "Space Mono";
+    font-size: 11px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+  & > span {
+    color: ${(props) => (props.themee ? "#2b3442" : "#fff")};
+    text-align: center;
+    font-family: "Space Mono";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-transform: uppercase;
+  }
 `;
 const ReposFolowersCon = styled.div<{ themee: boolean }>`
   grid-column: 1/5;
@@ -125,7 +192,7 @@ const InfoMainCon = styled.div<{ themee: boolean }>`
   border-radius: 15px;
   background: ${(props) => (props.themee ? "#fefefe" : "#1E2A47")};
   box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.2);
-  padding: 3.3rem 2.4rem 2rem 2.4rem;
+  padding: 3.3rem 2.4rem 4.8rem 2.4rem;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(4, 1fr);
